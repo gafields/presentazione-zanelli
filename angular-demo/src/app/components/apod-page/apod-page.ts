90de57433e34b29b516b2637be8234a16ae1dd46
gafields <62207718+gafields@users.noreply.github.com>
2026-02-22 23:39:20 +0100

    feat: Enhance APOD and Rover pages with data fetching and loading states

--- angular-demo/src/app/components/apod-page/apod-page.ts
+++ angular-demo/src/app/components/apod-page/apod-page.ts
@@ -1,14 +1,50 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { httpResource } from '@angular/common/http';
+import { API_KEY } from '../../constants';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+type ApodResponse = {
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+};
 
 @Component({
   selector: 'app-apod-page',
-  imports: [MatCardModule, MatButtonModule, MatIconModule, MatProgressSpinnerModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatInputModule,
+    FormsModule,
+  ],
   templateUrl: './apod-page.html',
   styleUrl: './apod-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ApodPage {}
+export class ApodPage {
+  public date = new Date().toISOString().split('T')[0];
+
+  public date$ = signal(this.date);
+
+  public request = httpResource<ApodResponse>(
+    () => `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&date=${this.date$()}`,
+  );
+
+  public isLoading = this.request.isLoading;
+  public data = this.request.value;
+
+  public onDateChanged(e: unknown) {
+    this.date$.set(this.date);
+  }
+}
--- angular-demo/src/app/components/rover-page/rover-page.ts
+++ angular-demo/src/app/components/rover-page/rover-page.ts
@@ -4,6 +4,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { API_KEY } from '../../constants';
+import { httpResource } from '@angular/common/http';
+
+export type RoverResponse = any; // Define the type based on the actual API response structure
 
 @Component({
   selector: 'app-rover-page',
@@ -18,4 +22,11 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
   styleUrl: './rover-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class RoverPage {}
+export class RoverPage {
+  public request = httpResource<RoverResponse>(
+    () =>
+      `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2015-6-3&api_key=${API_KEY}`,
+  );
+
+  public data = this.request.value;
+}
