08fb9012a7dc68ccbcdb0baaea64793539a75969
gafields <62207718+gafields@users.noreply.github.com>
2026-02-27 00:14:28 +0100

    feat: integrate date navigator component for date selection in NeowsPage

--- angular-demo/src/app/components/neows-page/neows-page.ts
+++ angular-demo/src/app/components/neows-page/neows-page.ts
@@ -6,7 +6,7 @@ import { API_KEY } from '../../constants';
 import { AsteroidInfo } from './asteroide/asteroide';
 import { PageHeader } from '../page-header/page-header';
 import { NeoObject } from '../../types/neo-object.model';
-import { FormsModule } from '@angular/forms';
+import { DateNavigator } from '../date-navigator/date-navigator';
 
 export type NeoWsResponse = {
   element_count: number;
@@ -15,33 +15,28 @@ export type NeoWsResponse = {
 
 @Component({
   selector: 'app-neows-page',
-  imports: [MatIconModule, MatProgressSpinnerModule, AsteroidInfo, PageHeader, FormsModule],
+  imports: [MatIconModule, MatProgressSpinnerModule, AsteroidInfo, PageHeader, DateNavigator],
   templateUrl: './neows-page.html',
   styleUrl: './neows-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NeowsPage {
-  public readonly today = new Date().toISOString().split('T')[0];
-  public date$ = signal(this.today);
+  public readonly date = signal(new Date().toISOString().split('T')[0]);
 
-  public request = httpResource<NeoWsResponse>(
+  public readonly request = httpResource<NeoWsResponse>(
     () =>
-      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${this.date$()}&end_date=${this.date$()}&api_key=${API_KEY}`,
+      `https://api.nasa.gov/neo/rest/v1/feed?start_date=${this.date()}&end_date=${this.date()}&api_key=${API_KEY}`,
   );
 
-  public isLoading = this.request.isLoading;
+  public readonly isLoading = this.request.isLoading;
 
-  public asteroids = computed(() => {
+  public readonly asteroids = computed(() => {
     const data = this.request.value();
     if (!data) return [];
     return Object.values(data.near_earth_objects).flat();
   });
 
-  public hazardousCount = computed(
+  public readonly hazardousCount = computed(
     () => this.asteroids().filter((a) => a.is_potentially_hazardous_asteroid).length,
   );
-
-  public onDateChanged(newDate: string) {
-    this.date$.set(newDate);
-  }
 }
