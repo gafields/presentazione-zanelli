eced22302d5a30f214df7181e8229b97489d9d20
gafields <62207718+gafields@users.noreply.github.com>
2026-02-26 12:00:56 +0100

    feat: remove unused RouterLink import from Welcome component and add .gitkeep to prompts directory

--- angular-demo/src/app/components/welcome/welcome.ts
+++ angular-demo/src/app/components/welcome/welcome.ts
@@ -1,5 +1,4 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,7 +6,7 @@ import { Card } from '../card/card';
 
 @Component({
   selector: 'app-welcome',
-  imports: [RouterLink, MatCardModule, MatButtonModule, MatIconModule, Card],
+  imports: [MatCardModule, MatButtonModule, MatIconModule, Card],
   templateUrl: './welcome.html',
   styleUrl: './welcome.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
